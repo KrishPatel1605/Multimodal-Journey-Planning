5368353eca01466a45bd12e7ad616b2d4b5aa6d0
Krish <krishpatel160512@gmail.com>
2026-02-06 22:00:11 +0530

    url update

--- Frontend/src/Pages/MainPage.jsx
+++ Frontend/src/Pages/MainPage.jsx
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import InputLayout from "../Component/InputLayout";
 import MapLeaflet from "../Component/MapLeaflet";
 import JourneyList from "../Component/JourneyList";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const MainPage = () => {
   const [from, setfrom] = useState('')
-  const [to, setto] =  useState('')
+  const [to, setto] = useState('')
   const [routes, setRoutes] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -13,19 +14,19 @@ const MainPage = () => {
   const [hasSearched, setHasSearched] = useState(false);
 
   const handleFromUpdate = (newFromData) => {
-      setfrom(newFromData);
-    };
+    setfrom(newFromData);
+  };
 
-    const handleToUpdate = (newToData) => {
-      setto(newToData);
-    };
+  const handleToUpdate = (newToData) => {
+    setto(newToData);
+  };
 
   const handleSearch = async (searchData) => {
     setLoading(true);
     setError(null);
     setSelectedRoute(null);
     setHasSearched(true);
-    
+
     try {
       console.log("Sending search request:", searchData);
 
@@ -35,7 +36,8 @@ const MainPage = () => {
         transportModes: searchData.transportModes
       };
 
-      const response = await fetch("http://localhost:5000/api/routes", {
+
+      const response = await fetch(`${API_BASE_URL}/api/routes`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -86,11 +88,11 @@ const MainPage = () => {
     <div className="flex h-screen bg-gray-50 font-sans">
       <div className="flex flex-col h-full w-[45%] bg-white shadow-lg border border-gray-100 overflow-hidden mr-6">
         <div className="p-3 border-b border-gray-200">
-          <InputLayout 
-          onSearch={handleSearch} 
-          onFromUpdate={handleFromUpdate} 
-          onToUpdate={handleToUpdate} 
-          loading={loading} />
+          <InputLayout
+            onSearch={handleSearch}
+            onFromUpdate={handleFromUpdate}
+            onToUpdate={handleToUpdate}
+            loading={loading} />
         </div>
 
         <div className="flex-1 overflow-y-auto p-3 pt-2">
